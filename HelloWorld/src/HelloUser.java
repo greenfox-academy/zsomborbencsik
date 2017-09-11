7905e7a4bf3a6ac053175e172205b3617e977676
zsombor.bencsik <bezso10@gmail.com>
2017-09-11 16:07:18 +0200

    hellouser

--- HelloWorld/src/HelloUser.java
+++ HelloWorld/src/HelloUser.java
@@ -6,10 +6,9 @@ public class HelloUser {
         Scanner scan = new Scanner (System.in);
         System.out.println("Enter your name: ");
         String user_name = scan.next();
-        System.out.println("The entered name is: ");
-        System.out.print("The entered name is: ");
+        System.out.println("Welcome " + user_name );
         // Modify this program to greet user instead of the World!
         // The program should ask for the name of the user
-        System.out.println("Hello, World!");
+
     }
 }
