4d95fa2d1d29fe2f3e4028f06b1ff97d10b8d917
zsombor.bencsik <bezso10@gmail.com>
2017-09-12 17:33:32 +0200

    Double Items stilll that bloodddy for loop

---
+++ Day2/src/DoubleItems.java
@@ -0,0 +1,15 @@
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] ag = {3, 4, 5, 6, 7};
+
+        for (int i = 0; i < ag.length; i++) {
+            System.out.println(ag[i] * 2);
+        }
+
+    }
+}
+
+
+// - Create an array variable named `ag`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
