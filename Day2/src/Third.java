6a4bb8c30f344dfca855f86eb45beda6102d6d88
zsombor.bencsik <bezso10@gmail.com>
2017-09-12 15:52:53 +0200

    Third

---
+++ Day2/src/Third.java
@@ -0,0 +1,11 @@
+public class Third {
+    public static void main(String[] args) {
+    int[] myNumbers = {4,5,6,7};
+
+        System.out.println("The third element is " + myNumbers[2]);
+    }
+}
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
