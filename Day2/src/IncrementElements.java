244f1e8bb8bce30504603eec09ca5f8ab8482672
zsombor.bencsik <bezso10@gmail.com>
2017-09-12 16:11:17 +0200

    Increment Elements

---
+++ Day2/src/IncrementElements.java
@@ -0,0 +1,9 @@
+public class IncrementElements {
+    public static void main(String[] args) {
+        int[] t = {1,2,3,4,5};
+        t[2] = ++ t[2];
+        System.out.println(t[2]);
+
+    }
+}
+
