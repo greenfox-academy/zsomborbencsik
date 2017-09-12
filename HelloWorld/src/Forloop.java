e34cea70b51ae87bcd2fa30e160120d4eaaf6bc6
zsombor.bencsik <bezso10@gmail.com>
2017-09-12 13:23:27 +0200

    Doubling

---
+++ HelloWorld/src/Forloop.java
@@ -0,0 +1,5 @@
+public class Forloop {
+    public static void main(String[] args) {
+        System.out.println(4.);
+    }
+}
---
+++ HelloWorld/src/Functions.java
@@ -0,0 +1,10 @@
+public class Functions {
+    public static void main(String[] args) {
+        myAwesomeFunction();
+
+    }
+
+    public static void myAwesomeFunction() {
+        System.out.println("Yoo whaduppp");
+    }
+}
