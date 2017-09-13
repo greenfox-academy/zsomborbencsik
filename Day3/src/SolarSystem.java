d3c8f0318d8c735aa2e7b8013acb1acdfea810b7
zsombor.bencsik <bezso10@gmail.com>
2017-09-13 13:12:46 +0200

    Solar System

---
+++ Day3/src/SolarSystem.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SolarSystem{
+    public static void main(String... args){
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+        planetList.add(5, "Saturn");
+
+        // Saturn is missing from the planetList
+        // Insert it into the correct position
+
+        System.out.println(planetList);
+    }
+}
