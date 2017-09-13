6e34d66a650ea311ae8ee7cfe5d50d913e2cba9f
zsombor.bencsik <bezso10@gmail.com>
2017-09-13 11:21:52 +0200

    UrlFixerv2

--- Day3/src/UrlFixer.java
+++ Day3/src/UrlFixer.java
@@ -1,12 +1,10 @@
 public class UrlFixer {
     public static void main(String... args){
         String url = "https//www.reddit.com/r/nevertellmethebots";
-
-        url = url.substring(0, 5) + ":" + url.substring(5, url.length());
+        
         url = url.replace("bots", "odds");
 
-        
-
+        url = new StringBuilder(url).insert(5, ":").toString();
 
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
         // Also, the URL is missing a crutial component, find out what it is and insert it too!
