b8deb3734f7472485e2fb08ca8814693b71e96f6
zsombor.bencsik <bezso10@gmail.com>
2017-09-13 16:15:54 +0200

    Calculator

---
+++ Day3/src/Calculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String... args) {
+        int a, b, answer;
+        String operator;
+
+        Scanner scan = new Scanner(System.in);
+        
+
+        log("Please type in the expression: ");
+        operator = scan.next();
+        a = scan.nextInt();
+        b = scan.nextInt();
+        if (operator.contains("+")) {
+            answer = a + b;
+            log(""+answer);
+        }
+        if (operator.contains("-")) {
+            answer = a - b;
+            log(""+answer);
+        }
+        if (operator.contains("*")) {
+            answer = a * b;
+            log(""+answer);
+        }
+        if (operator.contains("/")) {
+            answer = a / b;
+            log(""+answer);
+        }
+
+    }
+    public static void log (String scan) {
+        System.out.print(scan);
+    }
+}
+
+// Create a simple calculator application which reads the parameters from the prompt
+// and prints the result to the prompt.
+// It should support the following operations:
+// +, -, *, /, % and it should support two operands.
+// The format of the expressions must be: {operation} {operand} {operand}.
+// Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+// You can use the Scanner class
+// It should work like this:
+
+// Start the program
+// It prints: "Please type in the expression:"
+// Waits for the user input
+// Print the result to the prompt
+// Exit
