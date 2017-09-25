26e53d3d232148d24fb1aff80e1aac7065017b27
zsombor.bencsik <bezso10@gmail.com>
2017-09-25 11:47:20 +0200

    PostIt

---
+++ week-4/day-1/src/PostIt.java
@@ -0,0 +1,23 @@
+public class PostIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+
+    public static void main(String[] args) {
+        PostIt example1 = new PostIt();
+        example1.textColor = "blue";
+        example1.backgroundColor = "red";
+        example1.text = "niceeeee";
+        PostIt example2 = new PostIt();
+        example2.backgroundColor = "purpleeee";
+        example2.text = "ohhh boiii";
+        example2.textColor = "pink";
+        PostIt example3 = new PostIt();
+        example3.backgroundColor = "black";
+        example3.text = "asussan";
+        example3.textColor = "orange";
+    }
+
+}
+
