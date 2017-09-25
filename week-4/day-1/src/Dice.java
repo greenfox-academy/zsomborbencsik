ac3db6144ec0b8ce31dc2e977aff04b272405c0d
zsombor.bencsik <bezso10@gmail.com>
2017-09-25 15:09:22 +0200

    Dice

---
+++ week-4/day-1/src/Dice.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Dice {
+    //    You have a Dice class which has 6 dice
+//    You can roll all of them with roll()
+//    Check the current rolled numbers with getCurrent()
+//    You can reroll with reroll()
+//    Your task is to get where all dice is a 6
+    int[] dices = new int[6];
+
+    public int[] roll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+        return dices;
+    }
+
+    public int[] getCurrent() {
+        return dices;
+    }
+
+    public int getCurrent(int i) {
+        return dices[i];
+    }
+
+    public void reroll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+    }
+
+    public void reroll(int k) {
+        dices[k] = (int) (Math.random() * 6) + 1;
+    }
+
+    public static void main(String[] args) {
+        Dice myDice = new Dice();
+        myDice.getCurrent();
+        myDice.roll();
+        myDice.getCurrent();
+        myDice.getCurrent(5);
+        myDice.reroll();
+        myDice.getCurrent();
+        myDice.reroll(4);
+        myDice.getCurrent();
+
+        for (int i = 0; i < myDice.getCurrent().length; i++) {
+            while (myDice.getCurrent()[i] != 6 ) {
+                myDice.reroll();
+            }
+            System.out.println(myDice.getCurrent()[i]);
+        }
+    }
+}
