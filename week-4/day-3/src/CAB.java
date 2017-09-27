50d8babd09b6b852700df2ad79f167078f3257d4
zsombor.bencsik <bezso10@gmail.com>
2017-09-27 17:37:15 +0200

    CAB without testing finished

--- week-4/day-3/src/CAB.java
+++ week-4/day-3/src/CAB.java
@@ -13,27 +13,44 @@ public class CAB {
     }
     public CAB() {
         Random rand = new Random();
-        this.randomnumber = rand.nextInt(9999) + 1000;
+        this.randomnumber = rand.nextInt(8888) + 1000;
     }
-    public int[] inttolist(int guess) {
-        for (int i = 0; i < 4; i++) {
-            int [] tempint = 
-        }
+    public ArrayList<Integer> inttolist(int guess) {
+        int temp = guess;
+        ArrayList<Integer> array = new ArrayList<Integer>();
 
-    public void compareLists(int[] input1, int[] input2) {
-        for (int i = 0; i < input1.length; i++) {
-            if (input1[i] == input2[i]) {
-                System.out.println("cow at " + i);
+            do {
+                array.add(temp % 10);
+                temp /= 10;
+            } while (temp > 0);
+            return  array;
+
+    }
+
+    public void compareLists(ArrayList<Integer> input1,ArrayList<Integer> input2) {
+        int cowcounter = 0;
+        int bullcounter = 0;
+        for (int i = 0; i < input1.size(); i++) {
+            if (input1.get(i) == input2.get(i)) {
+                cowcounter++;
+            }
+            if (input1.contains(input2.get(i))) {
+                bullcounter++;
+
             }
         }
+        System.out.println("You have " + cowcounter + " cows.");
+        System.out.println("You have " + bullcounter + " bulls.");
     }
 
     public static void main(String[] args) {
         CAB newcab = new CAB();
         Scanner myScanner = new Scanner(System.in);
+        System.out.println(newcab.randomnumber);
         System.out.println("Enter a 4 digit number: ");
         int user_guess = myScanner.nextInt();
-        int [] playerguess = newcab.inttolist(user_guess);
+        ArrayList<Integer> playerguess = newcab.inttolist(user_guess);
+        newcab.compareLists(playerguess, newcab.inttolist(newcab.randomnumber));
 
     }
 
