eb6cd11f85dfb2c12df045fe582dada86b253eb3
zsombor.bencsik <bezso10@gmail.com>
2017-10-03 17:38:30 +0200

    Wanderer working on the walls

--- week-5/day-2/src/Board.java
+++ week-5/day-2/src/Board.java
@@ -15,16 +15,34 @@ public class Board extends JComponent implements KeyListener {
     int testBoxY;
     int oneUnit;
     PositionedImage player;
+    int[][] matrix;
+
+
 
     public Board() {
-        testBoxX = 0;
-        testBoxY = 0;
+        testBoxX = 0 / 72;
+        testBoxY = 0 / 72;
+        matrix = new int[][]{
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
+                {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+                {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 1, 1, 0, 0, 0}
+        };
         oneUnit = 72;
         // set the size of your draw board
         setPreferredSize(new Dimension(720, 720));
         setVisible(true);
     }
-
+    public int[][] getPlayerPosition(PositionedImage player) {
+        int[][] playerPosition = new int[][]{{testBoxX,testBoxY}};
+        return playerPosition;
+    }
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
@@ -38,21 +56,6 @@ public class Board extends JComponent implements KeyListener {
             image.posX = 0;
             image.posY += 72;
         }
-        PositionedImage player = new PositionedImage("C:\\Users\\ATOM\\greenfox\\zsomborbencsik\\week-5\\day-2\\hero0.png", testBoxX, testBoxY);
-        player.draw(graphics);
-
-        final int[][] matrix = {
-                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
-                {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-                {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
-                {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
-                {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
-                {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
-                {0, 0, 0, 1, 0, 1, 1, 0, 0, 0}};
-
         int[][] wallposition = new int[10][10];
         for (int i = 0; i < 10; i++) {
             wallposition[i][0] = i * oneUnit;
@@ -63,44 +66,44 @@ public class Board extends JComponent implements KeyListener {
         for (int t = 0; t < matrix.length; t++) {
             for (int j = 0; j < matrix[t].length; j++) {
                 if(matrix[t][j] == 1) {
-                    PositionedImage wall = new PositionedImage("wall.png", wallposition[0][j], wallposition[t][0]);
+                    PositionedImage wall = new PositionedImage("C:\\Users\\ATOM\\greenfox\\zsomborbencsik\\week-5\\day-2\\wall.png", wallposition[0][j], wallposition[t][0]);
                     wall.draw(graphics);
                 }
             }
         }
-        int imageCount = 4;
-        Image[] imageStripes = new Image[imageCount];
-        for (int i = 0; i < imageCount; i++) {
-            try {
-                imageStripes[i] = ImageIO.read(new File(String.format("C:\\Users\\ATOM\\greenfox\\zsomborbencsik\\week-5\\day-2\\hero" +i +".png", i)));
-            } catch (IOException e) {
-            }
-        }
+
+        PositionedImage player = new PositionedImage("C:\\Users\\ATOM\\greenfox\\zsomborbencsik\\week-5\\day-2\\hero0.png", testBoxX*oneUnit, testBoxY*oneUnit);
+        player.draw(graphics);
+        getPlayerPosition(player);
+
+
+
+
     }
 
     // To be a KeyListener the class needs to have these 3 methods in it
     @Override
     public void keyTyped(KeyEvent e) {
-
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
     }
 
     // But actually we can use just this one for our goals here
     @Override
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP && testBoxY > 0) {
-            testBoxY -= 72;
-        } else if(e.getKeyCode() == KeyEvent.VK_DOWN && testBoxY <720 - oneUnit) {
-            testBoxY += 72;
-        } else if(e.getKeyCode() == KeyEvent.VK_LEFT && testBoxX > 0) {
-            testBoxX -= 72;
-        } else if(e.getKeyCode() == KeyEvent.VK_RIGHT && testBoxX <720 - oneUnit) {
-            testBoxX += 72;
-        }
+            if (e.getKeyCode() == KeyEvent.VK_UP && testBoxY != 0 && matrix[testBoxY][testBoxX] != 1) {
+                testBoxY -= 1;
+            } else if (e.getKeyCode() == KeyEvent.VK_DOWN && testBoxY < 10 && matrix[testBoxY][testBoxX] != 1 ) {
+                testBoxY += 1;
+            } else if (e.getKeyCode() == KeyEvent.VK_LEFT && testBoxX != 0) {
+                testBoxX -= 1;
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && testBoxX < 10) {
+                testBoxX += 1;
+            }
+
+
         // and redraw to have a new picture with the new coordinates
         repaint();
     }
