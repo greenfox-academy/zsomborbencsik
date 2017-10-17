82fed52d31661b694dbf6a16abc77374e5047eb6
zsombor.bencsik <bezso10@gmail.com>
2017-10-17 17:14:40 +0200

    Credit Card task in progress

---
+++ week-6/day-2/src/CreditCardApp.java
@@ -0,0 +1,27 @@
+import com.greenfox.bx.*;
+
+public class CreditCardApp implements CreditCardy {
+
+    public static void main(String[] args) {
+
+    }
+    @Override
+    public int getSumCVV() {
+        return 0;
+    }
+
+    @Override
+    public String getNameCardholder() {
+        return null;
+    }
+
+    @Override
+    public String getCodeAccount() {
+        return null;
+    }
+
+    @Override
+    public int cumeSumCVV(String codeAccount) {
+        return 0;
+    }
+}
---
+++ week-6/day-2/src/com/greenfox/bx/CreditCardy.java
@@ -0,0 +1,14 @@
+package com.greenfox.bx;
+
+public interface CreditCardy {
+
+    int getSumCVV();
+
+    String getNameCardholder();
+
+    String getCodeAccount();
+
+    int cumeSumCVV(String codeAccount); // computes codeAccount checksum
+
+    String toString (); //String.format("Name=%s CC#=%s CVV=%d");
+}
