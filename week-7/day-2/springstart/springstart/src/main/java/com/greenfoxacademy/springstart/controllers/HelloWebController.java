55dd66643d1a644c65db614959e1e1c958da79e0
zsombor.bencsik <bezso10@gmail.com>
2017-10-24 17:47:07 +0200

    getting the colors right

--- week-7/day-2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-7/day-2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -5,19 +5,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
     AtomicLong count2;
+    String [] hellos;
 
     public HelloWebController() {
         this.count2 = new AtomicLong(0);
+        this.hellos = new String[]{"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
     }
 
     @RequestMapping("/web/greeting")
     public String greeting(Model model, @RequestParam(value = "name") String name) {
         count2.getAndIncrement();
+        Random r = new Random();
+        int random = r.nextInt(hellos.length);
+
+        model.addAttribute("hello",this.hellos[random]);
         model.addAttribute("name",name);
         model.addAttribute("id",this.count2);
         return "greeting";
