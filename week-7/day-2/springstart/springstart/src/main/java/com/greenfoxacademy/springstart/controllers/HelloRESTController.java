ff88eb1f92b72e1f5d583e45a10addd7273f77a4
zsombor.bencsik <bezso10@gmail.com>
2017-10-24 14:19:45 +0200

    Atmoiclong learned

--- week-7/day-2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-7/day-2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+    AtomicLong count;
+
+    public HelloRESTController() {
+        this.count = new AtomicLong(0);
+    }
 
     @RequestMapping
     public Greeting greeting(@RequestParam("name") String name) {
-        return new Greeting(1,"Hello " + name);
+        this.count.getAndIncrement();
+        return new Greeting(count.intValue(),"Hello " + name);
     }
 }
